298f1f0503c1101b3c8b2ceb011a4f1d5dbd5801
Alexander Spanos <alexisspa9@gmail.com>
2018-03-22 15:41:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
       current_line << "#{index+1}. #{name}"
     end
-    return current_line
+    puts current_line
   else
  puts "The line is currently empty."
   end
