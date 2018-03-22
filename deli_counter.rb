74eb978f6f0fa64ad28257ecdf7fdeff8235e14b
Alexander Spanos <alexisspa9@gmail.com>
2018-03-22 15:42:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 0
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     katz_deli.each_with_index do |name, index|
       current_line << " #{index+1}. #{name}"
     end
