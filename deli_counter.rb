082249a1a2b75c56355c6d7a5c0f96af87b3b5ab
Alexander Spanos <alexisspa9@gmail.com>
2018-03-22 15:33:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length > 0
+    katz_delix.each_with_index.map do |name, index|
+      "#{index+1}: #{name}"
+    end
+  else
+ puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli,name)
+  position = katz_deli.length + 1
+  katz_deli.push(name)
+  "#{name}, position:#{position}"
+end
